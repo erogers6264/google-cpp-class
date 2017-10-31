5bc369b90fc0dee3b1e99bf038463eca26066ee6
Ethan Rogers <erogers6264@gmail.com>
2017-10-30 22:12:28 -0700

    Create file for program to compute class grade

---
+++ final.cpp
@@ -0,0 +1,23 @@
+// final.cpp, Ethan Rogers
+// Description: A program to compute the final grade
+// for a programming course
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int assignmentsAverage() {
+  int avg = 0;
+}
+
+int getInput(string prompt) {
+  
+}
+
+
+
+
+int main() {
+  
+  return 0;
+}
